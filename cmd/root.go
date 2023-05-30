a8fb0f6e5150b5d648dee0f11585668494fb8412
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2023-05-30 18:32:55 +0530

    docs: updated 'pms --help' docs
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -32,15 +32,14 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pms",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A project management tool",
+	Long: `PMS - Project Management System S̶y̶s̶t̶e̶m̶ Sucks
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+A TUI/CLI utility to manage your projects.`,
+Example: `  pms
+  pms --help
+  pms --version
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		m := charm.NewModel()
 		p := tea.NewProgram(m)
@@ -48,6 +47,7 @@ to quickly create a Cobra application.`,
 			cobra.CheckErr(err)
 		}
 	},
+	Version: "0.1.0-alpha",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
